90ff78bfc0c4d9b7b743a849e69324d0f42cf29e
Sunny <47844914+Aftabe@users.noreply.github.com>
2019-12-05 21:52:52 +1100

    js example two code added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// window object in js
+//  Window object example one
+var txt = 'Hello Window!';
+ var fun = function() {
+     var txt = 'Hello function';
+
+     alert(txt);
+ };
+ fun();
+// window object in js, exmaple two
 var txt = 'Hello Window!';
  var fun = function() {
      var txt = 'Hello function';
