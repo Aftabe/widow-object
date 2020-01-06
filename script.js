715e99a943fee772b40b7faa5a431637cea4b49f
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-01-06 21:26:21 +1100

    key words modified

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //  Window object example one
-const txt = 'Hello Window!';
- const fun = function() {
-     const txt = 'Hello function';
-     const txt = 'Hello function';
+var txt = 'Hello Window!';
+ var fun = function() {
+     var txt = 'Hello function';
+     var txt = 'Hello function';
 
      alert(txt);
      alert(window.txt);
