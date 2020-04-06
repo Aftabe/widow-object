0fed48cf8d7fda2649e977a68cbcf5b6dd699f2c
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-04-06 18:26:27 +1000

    code removed

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-//  Window object example one
-var txt = 'Hello Window!';
-var  fun = function() {
-     var txt = 'Hello function';
-     var txt = 'Hello function';
+// //  Window object example one
+// var txt = 'Hello Window!';
+// var  fun = function() {
+//      var txt = 'Hello function';
+//      var txt = 'Hello function';
 
-     alert(txt);
-     alert(window.txt);
- };
- fun();
+//      alert(txt);
+//      alert(window.txt);
+//  };
+//  fun();
 
  
-// window object in js, exmaple two
-var txt = 'Hello Window!';
- var fun = function() {
-     var txt = 'Hello function';
+// // window object in js, exmaple two
+// var txt = 'Hello Window!';
+//  var fun = function() {
+//      var txt = 'Hello function';
 
-     alert(txt);
- };
- fun();
+//      alert(txt);
+//  };
+//  fun();
