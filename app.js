e1abf684f53febd8a85f752767fb6365995106d6
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-05-07 21:16:46 +1000

    statements added

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 console.log('Logy!');
 }());
 
-let info = document.all[11].value;
+// let info = document.all[11].value;
 
 
 
 
 
 
-console.log(info);
+// console.log(info);
+
+
+
+const name = {
+    firstName: 'Ali',
+    lastName: 'Khaliq',
+    age: 20
+}
+console.log(name.firstName + name.age);
